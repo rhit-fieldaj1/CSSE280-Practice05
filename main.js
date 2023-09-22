83d56f02f32d30cbbd8c2fd769206e865f07b33b
Adam Field <98504874+rhit-fieldaj1@users.noreply.github.com>
2023-09-22 04:58:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// Statements
+let x, y, z;
+x = 5;
+y = 9;
+z = x * y;
+
+// operators
+
+x += 8;
+y *= 2;
+z %= 4;
+
+if(x < 20 && y > 3) {
+    z = 0;
+}
+
+z++;
+
+let name = "Johnny Cash";
+
+// Functions
+function changeHeader() {
+    document.getElementById("mainHeading").innerHTML = "New Heading";
+}
+
+/**
+ * Multi line comment
+ */
+
